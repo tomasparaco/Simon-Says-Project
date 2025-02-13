bcbcab2c8b4a309f259aa70f38d1998a542b9ac0
tomasparaco <tomas.paraco@correo.unimet.edu.ve>
2025-02-13 11:11:51 -0400

    Update simonDice.js
    Cambio de formato

--- Simon Says/simonDice.js
+++ Simon Says/simonDice.js
@@ -1,134 +1,107 @@
-const round = document.getElementById('round');
-const simonButtons = document.getElementsByClassName('square');
-const startButton = document.getElementById('startButton');
-
-class Simon {
-    constructor(simonButtons, startButton, round) {
-        this.round = 0;
-        this.userPosition = 0;
-        this.totalRounds = 10;
-        this.sequence = [];
-        this.speed = 1000;
-        this.blockedButtons = true;
-        this.buttons = Array.from(simonButtons);
-        this.display = {
-            startButton,
-            round
-        }
-        this.errorSound = new Audio('./sounds/error.wav');
-        this.buttonSounds = [
-            new Audio('./sounds/1.mp3'),
-            new Audio('./sounds/2.mp3'),
-            new Audio('./sounds/3.mp3'),
-            new Audio('./sounds/4.mp3'),
-        ]
-    }
-
-    // Inicia el Simon
-    init() {
-        this.display.startButton.onclick = () => this.startGame();
-    }
-
-    // Comienza el juego
-    startGame() {
-        this.display.startButton.disabled = true; 
-        this.updateRound(0);
-        this.userPosition = 0;
-        this.sequence = this.createSequence();
-        this.buttons.forEach((element, i) => {
-            element.classList.remove('winner');
-            element.onclick = () => this.buttonClick(i);
-        });
-        this.showSequence();
-    }
-
-    // Actualiza la ronda y el tablero
-    updateRound(value) {
-        this.round = value;
-        this.display.round.textContent = `Round ${this.round}`;
-    }
-
-    // Crea el array aleatorio de botones
-    createSequence() {
-        return Array.from({length: this.totalRounds}, () =>  this.getRandomColor());
-    }
-
-    // Devuelve un número al azar entre 0 y 3
-    getRandomColor() {
-        return Math.floor(Math.random() * 4);
-    }
-
-    // Ejecuta una función cuando se hace click en un botón
-    buttonClick(value) {
-        !this.blockedButtons && this.validateChosenColor(value);
-    }
-
-    // Valida si el boton que toca el usuario corresponde a al valor de la secuencia
-    validateChosenColor(value) {
-        if(this.sequence[this.userPosition] === value) {
-            this.buttonSounds[value].play();
-            if(this.round === this.userPosition) {
-                this.updateRound(this.round + 1);
-                this.speed /= 1.02;
-                this.isGameOver();
-            } else {
-                this.userPosition++;
-            }
-        } else {
-            this.gameLost();
-        }
-    }
-
-    // Verifica que no haya acabado el juego
-    isGameOver() {
-        if (this.round === this.totalRounds) {
-            this.gameWon();
-        } else {
-            this.userPosition = 0;
-            this.showSequence();
-        };
-    }
-
-    // Muestra la secuencia de botones que va a tener que tocar el usuario
-    showSequence() {
-        this.blockedButtons = true;
-        let sequenceIndex = 0;
-        let timer = setInterval(() => {
-            const button = this.buttons[this.sequence[sequenceIndex]];
-            this.buttonSounds[this.sequence[sequenceIndex]].play();
-            this.toggleButtonStyle(button)
-            setTimeout( () => this.toggleButtonStyle(button), this.speed / 2)
-            sequenceIndex++;
-            if (sequenceIndex > this.round) {
-                this.blockedButtons = false;
-                clearInterval(timer);
-            }
-        }, this.speed);
-    }
-
-    // Pinta los botones para cuando se está mostrando la secuencia
-    toggleButtonStyle(button) {
-        button.classList.toggle('active');
-    }
-
-    // Actualiza el simon cuando el jugador pierde
-    gameLost() {
-        this.errorSound.play();
-        this.display.startButton.disabled = false; 
-        this.blockedButtons = true;
-    }
-
-    // Muestra la animacón de triunfo y actualiza el simon cuando el jugador gana
-    gameWon() {
-        this.display.startButton.disabled = false; 
-        this.blockedButtons = true;
-        this.buttons.forEach(element =>{
-            element.classList.add('winner');
-        });
-        this.updateRound('🏆');
-    }
+let divs = Array.from(document.querySelectorAll('#container div'));
+let parent = document.getElementById('parent'); let button = document.getElementById('button'); let gameArray, gameArrayIndex, score, gameOver; let highestScore = 0; let close = document.getElementById('close'); let currentScr = document.getElementById('currentScr'); let highScr = document.getElementById('highScr'); let title = document.getElementById('title');
+startGame = () => {
+    gameOver = false; gameArray = []; gameArrayIndex = 0; score = 0;
+    divs.forEach(e => {
+        e.style.cursor = 'pointer';
+        e.addEventListener('click', eventlisten);
+    })
+    let random = Math.floor(Math.random() * divs.length);
+    gameArray.push(random);
+    setTimeout(() => { divs[random].classList.add('flash'); }, 200);
+    setTimeout(() => { divs[random].classList.remove('flash'); }, 500);
 }
 
-const simon = new Simon(simonButtons, startButton, round);
-simon.init();
+function eventlisten() {
+    this.classList.add('flash');
+    setTimeout(() => { this.classList.remove('flash'); }, 200);
+    checkWin(parseInt(this.textContent));
+}
 
+async function gameAutomate() {
+    gameArrayIndex = 0;
+    if (gameArray.length != 0) { await setColor(); }
+    let random = Math.floor(Math.random() * divs.length);
+    gameArray.push(random);
+    divs[random].classList.add('flash');
+    setTimeout(() => { divs[random].classList.remove('flash'); }, 200);
+}
+
+checkWin = (boxText) => {
+    (gameArray[gameArrayIndex] != boxText) ? (resetGame()) : ({});
+    if (gameArrayIndex == gameArray.length - 1 && !gameOver) {
+        setTimeout(() => {
+            score++;
+            (score > highestScore) ? (highestScore = score) : ({});
+            gameAutomate();
+        }, 500);
+    }
+    if (gameArray.length != 0) { gameArrayIndex++ };
+}
+
+setColor = () => {
+    return new Promise(async (resolve, reject) => {
+        divs.forEach(e => {
+            e.removeEventListener('click', eventlisten);
+            e.style.cursor = 'default';
+        })
+        for (let i = 0; i < gameArray.length; i++) {
+            divs[gameArray[i]].classList.add('flash');
+            await removeColor(i);
+            await changeColor();
+            if (i == gameArray.length - 1) {
+                divs.forEach(e => {
+                    e.addEventListener('click', eventlisten);
+                    e.style.cursor = 'pointer';
+                })
+                resolve(true);
+            };
+        }
+    })
+};
+
+removeColor = (index) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            divs[gameArray[index]].classList.remove('flash');
+            resolve(true);
+        }, 200);
+    })
+};
+
+changeColor = () => { return new Promise((resolve, reject) => { setTimeout(() => { resolve(true); }, 200); }) }
+
+function resetGame() {
+    gameOver = true;
+    divs.forEach(e => {
+        e.removeEventListener('click', eventlisten);
+        e.style.cursor = 'default';
+    });
+    let curscr = Array.from(document.querySelectorAll('#currentScr div')).length; let highscr = Array.from(document.querySelectorAll('#highScr div')).length;
+    addElements(score, curscr, currentScr); addElements(highestScore, highscr, highScr);
+    inst.innerHTML = '<div id="gameFinished">Game Over!</div><i class="fa-solid fa-xmark" id="close"></i>';
+    inst.classList.add('gameOver');
+    inst.style.transform = 'translate(-50%,-50%) scale(1)';
+    close = document.getElementById('close');
+    close.addEventListener('click', scale);
+}
+
+function addElements(a, b, c) {
+    if (a > (b - 1)) {
+        for (let i = b; i <= a; i++) {
+            let x = document.createElement('div');
+            x.innerText = i;
+            c.appendChild(x);
+        }
+    };
+    c.style.transform = `translateY(${-50 * a}px)`;
+}
+close.addEventListener('click', scale);
+
+function scale() {
+    inst.style.transform = 'translate(-50%, -50%) scale(0)';
+    parent.style.transform = 'scale(1)';
+    button.style.transform = 'scale(1)';
+    currentScr.style.transform = '';
+    title.style.opacity = '0.5';
+}
